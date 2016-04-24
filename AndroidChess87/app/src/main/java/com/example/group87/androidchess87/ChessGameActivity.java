ec087915ff8b97930f6a7ff2621cec1f1de4f7f9
Danny <choidanny94@gmail.com>
2016-04-24 15:03:29 -0400

    init

---
+++ AndroidChess87/app/src/main/java/com/example/group87/androidchess87/ChessGameActivity.java
@@ -0,0 +1,13 @@
+package com.example.group87.androidchess87;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChessGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chess_main);
+    }
+}
